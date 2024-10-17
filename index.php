8a77f0f6507a02c2099bd7279de7f0103c18ddf4
liamdiaz869 <williamdiaz0869@gmail.com>
2024-10-16 21:30:07 -0300

    ns

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My Little Wiki</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="fondo_pag">
+<header>
+    <div>
+    <div>
+        <img src="img/My-Little-Pony-Logo-2021-500x293.webp" class="logo_mlp">
+            <div>
+            
+    <nav>
+        <ul>
+            
+            <li class="item_nav"><a href="#" class="link_nav">Inicio</a></li>
+            <li class="item_nav"><a href="#" class="link_nav">Acerca</a></li>
+            <li class="item_nav"><a href="#" class="link_nav">Servicios</a></li>
+            <li class="item_nav"><a href="#" class="link_nav">Contacto</a></li>
+            <li>
+                <img src="img/img perfil.webp" class="img_perfil">
+            </li>
+        </ul>
+    </nav>
+</div>
+</header>
     
 </body>
 </html>
