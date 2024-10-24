939a19847b57243393b981888833aff300a5a684
mili sanchez <milisanchezcaruso@gmail.com>
2024-10-24 13:20:20 -0300

    subo pagina completa

---
+++ Fluttershy.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my little wiki</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="fondo_pag">
+<header>
+    <div class="header-container">
+        <a href="index.php">
+            <img src="img/my-little-pony-logo-2021-500x293.webp" class="logo_mlp" alt="my little pony logo">
+        </a>
+        <nav>
+            <ul class="nav-list">
+                <li class="item_nav"><a href="index.php" class="link_nav">inicio</a></li>
+                <li class="item_nav"><a href="#" class="link_nav">temporadas</a></li>
+                <li class="item_nav"><a href="#" class="link_nav">películas</a></li>
+                <li class="item_nav"><a href="#" class="link_nav">canciones</a></li>
+            </ul>
+        </nav>
+        <img src="img/img_perfil.jpg" class="img_perfil" alt="imagen de perfil">
+    </div>
+</header>
+
+<main>
+    <div class="content-container">
+        <div class="sidebar-left">
+            <a href="Twilight.php">
+                <img src="img/img_twilight.jpg" alt="personaje 1">
+            </a>
+            <a href="PinkiePie.php">
+                <img src="img/img_pinkie.jpg" alt="personaje 2">
+            </a>
+            <a href="Rarity.php">
+                <img src="img/img_rarity.jpg" alt="personaje 3">
+            </a>
+            <a href="Fluttershy.php">
+                <img src="img/img_fluttershy.jpg" alt="personaje 4">
+            </a>
+            <a href="RainbowDash.php">
+                <img src="img/img_rainbow.jpg" alt="personaje 5">
+            </a>
+            <a href="AppleJack.php">
+                <img src="img/img_applejack.jpg" alt="personaje 6">
+            </a>
+        </div>
+
+        <section class="main-content">
+            <h1>Rarity</h1>
+            <div class="personaje-contenedor">
+                <div class="personajes">
+                    <img src="img/img_rarity1.jpg" alt="rarity">
+                </div>
+                <div class="texto">
+                    <p><strong>Nombre:</strong> Rarity <br> 
+                       <strong>Elemento de la armonía:</strong> Generosidad <br>
+                       <strong>Ocupación:</strong> Modista <br>
+                       <strong>Vive en:</strong> Boutique Carrousel - Ponyville <br>
+                       <strong>Tipo de pony:</strong> Unicornio 
+                    </p>
+                </div>
+                
+            </div>
+            <p class="personajes_text">
+                Rarity es una pony unicornio que reside en Ponyville y es una de los principales personajes de My Little Pony; La Magia de la Amistad. Su sueño es vivir en Canterlot. Destaca por su belleza, su pelo brillante y sus extraordinarios rizos de color morado. Ella es la hermana mayor de Sweetie Belle y quizás el amor platónico de Spike, ya que en el episodio Simple Ways, se ve que Spike perdió un poco el interés hacia ella. Rarity es una diseñadora de moda y como costurera en su propia tienda, Boutique del carrusel. Tiene una gata persa blanco llamada Opalescence. Ella representa el Elemento de la Generosidad. A diferencia de Twilight Sparkle, su magia se utiliza para las actividades relacionadas con su talento especial y para tareas básicas como sostener y transportar objetos o la búsqueda de piedras preciosas.
+            </p>
+            <div class= "personajes-relacionados">
+                <p>Personajes relacionados:</p>
+                    <div class= "relacionados">
+                        <img src="img/img_pinkie1.jpg" alt="pinkie">
+                    <p>Pinkie Pie</p>
+                    </div>
+            </div>
+        </section>
+
+      
+        <div class="sidebar-right">
+            <h2>podría interesarte...</h2>
+            <div class="suggestion">
+                <img src="img/img_pinkiee.jpg" alt="pinkie">
+                <p>¡ULTIMO MOMENTO!</p>
+            </div>
+            <div class="suggestion">
+                <img src="img/pinkie.jpg" alt="titulo 2">
+                <p>¡ULTIMO MOMENTO!</p>
+            </div>
+            <div class="suggestion">
+                <img src="img/apple.jpg" alt="titulo 3">
+                <p>¡ULTIMO MOMENTO!</p>
+            </div>
+        </div>
+    
+</main>
+<footer class="footer">
+    <br>
+    <p>&copy; 2024 My Little Pony. Todos los derechos reservados.</p>
+</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,26 +7,78 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="fondo_pag">
-<header>
-    <div>
-    <div>
-        <img src="img/My-Little-Pony-Logo-2021-500x293.webp" class="logo_mlp">
-            <div>
-            
-    <nav>
-        <ul>
-            
-            <li class="item_nav"><a href="#" class="link_nav">Inicio</a></li>
-            <li class="item_nav"><a href="#" class="link_nav">Acerca</a></li>
-            <li class="item_nav"><a href="#" class="link_nav">Servicios</a></li>
-            <li class="item_nav"><a href="#" class="link_nav">Contacto</a></li>
-            <li>
-                <img src="img/img perfil.webp" class="img_perfil">
-            </li>
-        </ul>
-    </nav>
-</div>
-</header>
-    
+    <header>
+        <div class="header-container">
+            <a href="index.php">
+                <img src="img/my-little-pony-logo-2021-500x293.webp" class="logo_mlp" alt="my little pony logo">
+            </a>
+            <nav>
+                <ul class="nav-list">
+                    <li class="item_nav"><a href="index.php" class="link_nav">Inicio</a></li>
+                    <li class="item_nav"><a href="#" class="link_nav">Temporadas</a></li>
+                    <li class="item_nav"><a href="#" class="link_nav">Películas</a></li>
+                    <li class="item_nav"><a href="#" class="link_nav">Canciones</a></li>
+                </ul>
+            </nav>
+            <img src="img/img_perfil.jpg" class="img_perfil" alt="Imagen de perfil">
+        </div>
+    </header>
+
+    <main>
+        <div class="content-container">
+            <div class="sidebar-left">
+                <a href="Twilight.php">
+                    <img src="img/img_twilight.jpg" alt="Personaje 1">
+                </a>
+                <a href="PinkiePie.php">
+                    <img src="img/img_pinkie.jpg" alt="Personaje 2">
+                </a>
+                <a href="Rarity.php">
+                    <img src="img/img_rarity.jpg" alt="Personaje 3">
+                </a>
+                <a href="Fluttershy.php">
+                    <img src="img/img_fluttershy.jpg" alt="Personaje 4">
+                </a>
+                <a href="RainbowDash.php">
+                    <img src="img/img_rainbow.jpg" alt="Personaje 5">
+                </a>
+                <a href="AppleJack.php">
+                    <img src="img/img_applejack.jpg" alt="Personaje 6">
+                </a>
+            </div>
+
+            <section class="main-content">
+                <h1>Inicio</h1>
+                <div class="characters-banner">
+                    <img src="img/banner_mlp.jpg" alt="Personajes My Little Pony">
+                </div>
+                <p class= "parrafo1"><u>¡Gracias por interesarte en nuestro contenido!</u></p>
+                <p class= "parrafo2">Veremos información sobre la serie, las películas, los personajes principales e incluso las canciones.</p>
+                <p class= "parrafo3">Habrá enlaces que dirijan hacia dónde ver <br> cada temporada.</p>
+                <p class= "parrafo4">¡También dejaremos un par de noticias de esta serie que podrían interesarte!</p>
+            </section>
+
+            <div class="sidebar-right">
+                <h2>Podría interesarte...</h2>
+                <div class="suggestion">
+                    <img src="img/img_pinkiee.jpg" alt="Pinkie">
+                    <p>¡Último momento!</p>
+                </div>
+                <div class="suggestion">
+                    <img src="img/pinkie.jpg" alt="Título 2">
+                    <p>¡Último momento!</p>
+                </div>
+                <div class="suggestion">
+                    <img src="img/apple.jpg" alt="Título 3">
+                    <p>¡Último momento!</p>
+                </div>
+            </div>
+        </div>
+    </main> 
+
+    <footer class="footer">
+        <br>
+        <p>&copy; 2024 My Little Pony. Todos los derechos reservados.</p>
+    </footer>
 </body>
 </html>
