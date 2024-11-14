d6a835db85900f611e8da185a03a26d2509c8f33
milisanchez <milisanchezcaruso@gmail.com>
2024-11-14 15:13:57 -0300

    ooeea

---
+++ agregar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Little Wiki</title>
+        <link rel="icon" href="img/My-Little-Pony-Logo-2021-500x293.webp" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="fondo_form">
+<div class="agregar">
+        <h2 class="sesion">Subir temporada</h2>
+        <form action="" method="POST">
+            <p class="text-mo">Temporada</p>
+            <input type="text" placeholder="1,2,3,4..." class="form" name="tempo"> 
+            <p class="text-mo">Inicio</p>
+            <input type="text" placeholder="0000-00-00" class="form" name="inicio">
+            <p class="text-mo">Final</p>
+            <input type="text" placeholder="0000-00-00" class="form" name="final">
+        </form>
+            <input class="ingresa" type="submit" value="Agregar"></in>
+
+            </div>
+</body>
+</html>
---
+++ elimina.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Little Wiki</title>
+        <link rel="icon" href="img/My-Little-Pony-Logo-2021-500x293.webp" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="fondo_form">
+<div class="agregar">
+        <h2 class="sesion">Eliminar temporada</h2>
+        <form action="" method="POST">
+            <p class="text-mo">Temporada</p>
+            <input type="text" placeholder="1,2,3,4..." class="form" name="tempo"> 
+            <img src="img/pinki-triste.jpg" alt="triste" class="img-eliminated">
+            <input class="ingresa" type="submit" value="Eliminar"></in>
+
+            </div>
+</body>
+</html>
