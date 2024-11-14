68d83f277545f5271eb91d0dd71458cf848e3fb1
liamdiaz869 <williamdiaz0869@gmail.com>
2024-11-14 13:40:34 -0300

    bdd

--- bd/mlp.sql
+++ bd/mlp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2024 a las 18:35:08
+-- Tiempo de generación: 14-11-2024 a las 17:39:01
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,9 +39,15 @@ CREATE TABLE `temporadas` (
 --
 
 INSERT INTO `temporadas` (`inicio`, `final`, `temporada`, `id`) VALUES
-('2010-10-10', '2011-05-06', 0, 1),
-('2011-09-17', '2012-04-21', 0, 2),
-('2012-11-10', '2013-02-16', 0, 3);
+('2010-10-10', '2011-05-06', 1, 1),
+('2011-09-17', '2012-04-21', 2, 2),
+('2012-11-10', '2013-02-16', 3, 3),
+('2012-11-23', '2014-05-10', 4, 4),
+('2015-04-04', '2015-11-28', 5, 5),
+('2016-03-26', '2016-10-22', 6, 6),
+('2017-04-15', '2017-10-28', 7, 7),
+('2018-03-24', '2018-10-13', 8, 8),
+('2019-04-06', '2019-10-12', 9, 9);
 
 -- --------------------------------------------------------
 
@@ -85,7 +91,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `temporadas`
 --
 ALTER TABLE `temporadas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
