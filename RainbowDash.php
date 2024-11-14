55bf32ea77bf2d5f1afb174767d55061aaeaec3e
milisanchez <milisanchezcaruso@gmail.com>
2024-11-14 14:06:29 -0300

    oa

--- RainbowDash.php
+++ RainbowDash.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>my little wiki</title>
+    <title>My Little Wiki</title>
+        <link rel="icon" href="img/My-Little-Pony-Logo-2021-500x293.webp" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="fondo_pag">
